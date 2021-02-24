5486ec952ca23f7f665d464376acb53c26ba1202
Shubham Kumar <shubhamkumarpro1@gmail.om>
2021-02-25 01:48:42 +0530

    updated .vscode

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # map_stitch_package
+
+
+ROS package to align maps from different frames of references. 
+
+w
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/skpro19/catkin_ws/src/map_stitch_package/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/skpro19/catkin_ws/src/map_stitch_package/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/skpro19/catkin_ws/src/map_stitch_package/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/skpro19/catkin_ws/src/map_stitch_package:$ROS_PACKAGE_PATH"
